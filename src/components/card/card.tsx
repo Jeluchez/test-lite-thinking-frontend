c33b9029edc521ce697ff1808a3f1e34dd1c463b
jose luis sanchez <JoseL.Sanchez@Millicom.com>
2023-05-10 00:56:03 -0500

    feat: add company cards

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,8 +1,14 @@
-const Card = () => {
+import React from 'react'
+
+interface Props {
+  title: string
+  content: JSX.Element
+}
+export const Card: React.FC<Props> = ({ title, content }) => {
   return (
     <div className='card'>
-      <div className='card__title'></div>
-      <div className='card__content'></div>
+      <div className='card__title'>{title}</div>
+      <div className='card__content'>{content}</div>
     </div>
   )
 }
--- src/screens/companies/companiesScreen.tsx
+++ src/screens/companies/companiesScreen.tsx
@@ -1,12 +1,42 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { Card } from 'components/card/card'
+
 export const CompaniesScreen: React.FC = () => {
   return (
-    <div>
-      <header>
+    <div className='company'>
+      <header className='company__header'>
         <h1>crear compañia</h1>
-        <p>administre las compañias del inventario</p>
+        <p className='company__header-desc'>
+          administre las compañias del inventario
+        </p>
 
-        <button>Create</button>
+        <button className='header__btn-create'>
+          <FontAwesomeIcon icon={faPlusCircle} className='btn__create-icon' />
+          Crear
+        </button>
       </header>
+
+      <CompanyGrid />
+    </div>
+  )
+}
+
+const CompanyGrid: React.FC = () => {
+  const length = 8
+  const grid = Array.from({ length }, (_, index) => index + 1)
+
+  const content = (
+    <>
+      <div>La mejor empresa Retail</div>
+      <button>editar</button>
+    </>
+  )
+  return (
+    <div className='company__grid'>
+      {grid.map((company, index) => (
+        <Card title='Walmart' content={content} />
+      ))}
     </div>
   )
 }
